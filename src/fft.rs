b785121f058199b33a9b4b96d12375359f64bffe
Ken Butler <Ken.Butler.AE@gmail.com>
2021-01-16 23:46:52 -0500

    Completed unscrambling. Integration testing.

--- src/fft.rs
+++ src/fft.rs
@@ -2,7 +2,7 @@
 
 // Uses
 use log::{info, error, debug};
-use std::f32::consts::PI;
+use std::f64::consts::PI;
 use num_complex::Complex;
 
 /// Computes Fast Fourier Transform (FFT)
@@ -12,12 +12,14 @@ use num_complex::Complex;
 /// Existing crate: https://docs.rs/rustfft/5.0.1/rustfft/
 ///
 /// * `signal` - Complex signal
-pub fn fft(signal: Vec<Complex<f32>>) -> Vec<Complex<f32>> {
+pub fn fft(signal: Vec<Complex<f64>>, sampling_frequency: f64) -> (Vec<f64>, Vec<f64>) {
     info!("Entered FFT");
 
+    debug!("Signal length = {}", signal.len());
+
     // Split signal into real and imaginary portions
-    let mut real: Vec<f32> = vec![0.0; signal.len()];
-    let mut imag: Vec<f32> = vec![0.0; signal.len()];
+    let mut real: Vec<f64> = vec![0.0; signal.len()];
+    let mut imag: Vec<f64> = vec![0.0; signal.len()];
 
     for i in 0..signal.len() {
         real[i] = signal[i].re;
@@ -25,44 +27,60 @@ pub fn fft(signal: Vec<Complex<f32>>) -> Vec<Complex<f32>> {
     }
 
     scramble(&mut real, &mut imag);
-    unscramble(&real, &imag);
+    unscramble(&mut real, &mut imag);
 
-    let freq: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); signal.len()];
-    return freq;
+    // Set magnitudes
+    let mut magnitudes: Vec<f64> = vec![0.0; signal.len()];
+    for i in 0..real.len() {
+        magnitudes[i] = (real[i] * real[i] + imag[i] * imag[i]).sqrt();
+    }
+
+    // Set frequencies
+    let mut freq: Vec<f64> = vec![0.0; signal.len() / 2];
+    let stop = signal.len() * (sampling_frequency as usize / 2 - 1);
+//    let stop: usize = real.len() * ((sampling_frequency / 2.0) as usize - 2);
+    let step_size = 2 * ((sampling_frequency / 2.0) as usize) - 1;
+    debug!("freq size = {}, stop={}, step size={}", freq.len(), stop, step_size);
+    let mut idx = 0 as usize;
+    for i in (0..stop).step_by(step_size) {
+        freq[idx] = (i as f64) / (real.len() as f64);
+        idx += 1;
+    }
+    return (freq, magnitudes);
 }
 
 /// Scramble data
 ///
 /// * `x` - Vector of real values
 /// * `y` - Vector of imaginary values
-fn scramble(x: &mut Vec<f32>, y: &mut Vec<f32>) {
+fn scramble(x: &mut Vec<f64>, y: &mut Vec<f64>) {
     debug!("FFT: Scrambling");
     if x.len() != y.len() {
         error!("Real and imaginary vectors must be the same size.");
     }
     let n = x.len();
-    const BASE: f32 = 2.0;
-    let m = ((n as f32).ln() / BASE.ln()) as i32;
+    const BASE: f64 = 2.0;
+    let m = ((n as f64).ln() / BASE.ln()) as i32;
     let mut n2: usize = n;
     for _k in 1..m {
         let n1 = n2;
         n2 /= 2;
-        let mut angle: f32 = 0.0;
-        let arg: f32 = 2.0 * PI / (n1 as f32);
+        let mut angle: f64 = 0.0;
+        let arg: f64 = 2.0 * PI / (n1 as f64);
         for j in 0..n2 - 1 {
             let c = angle.cos();
             let s = -(angle.sin());
             for i in (j..n - 1).step_by(n1) {
                 let idx = i as usize;
                 let kk: usize = idx + n2;
-                let xt: f32 = x[idx] - x[kk];
+                let xt: f64 = x[idx] - x[kk];
                 x[idx] += x[kk];
-                let yt: f32 = y[idx] - y[kk];
+                let yt: f64 = y[idx] - y[kk];
                 y[idx] += y[kk];
                 x[kk] = xt * c - yt * s;
                 y[kk] = yt * c + xt * s;
             }
-            angle = ((j + 1) as f32) * arg;
+            angle = ((j + 1) as f64) * arg;
         }
     }
 }  // scramble()
@@ -71,10 +89,40 @@ fn scramble(x: &mut Vec<f32>, y: &mut Vec<f32>) {
 ///
 /// * `x` - Vector of real values
 /// * `y` - Vector of imaginary values
-fn unscramble(x: &mut Vec<f32>, y: &mut Vec<f32>) {
-    // TODO: Implement
+fn unscramble(x: &mut Vec<f64>, y: &mut Vec<f64>) {
     debug!("FFT: Unscrambling");
     if x.len() != y.len() {
         error!("Real and imaginary vectors must be the same size.");
+    }
+    let n = x.len() as i32;
+    let mut j = 0;
+    let mut k: i32;
+
+    for i in 0..n-2 {
+        if i < j {
+            // TODO: Is there a better way to swap values here?
+            let idx_i = i as usize;
+            let idx_j = j as usize;
+            let xt = x[idx_j];
+            x[idx_j] = x[idx_i];
+            x[idx_i] = xt;
+            let yt = y[idx_j];
+            y[idx_j] = y[idx_i];
+            y[idx_i] = yt;
+        }
+        k = n / 2;
+        loop {
+            if k >= j + 1 {
+                break;
+            }
+            j -= k;
+            k /= 2;
+        }
+        j += k;
+    }
+    for i in 0..n-1 {
+        let idx_i = i as usize;
+        x[idx_i] /= n as f64;
+        y[idx_i] /= n as f64;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod fft;
 
 // Uses
 use simple_logger::SimpleLogger;
-use log::{info};
-use std::f32::consts::PI;
+use log::{info, debug};
+use std::f64::consts::PI;
 use num_complex::Complex;
 
 fn main() {
@@ -15,17 +15,17 @@ fn main() {
     const SIGNAL_LENGTH: usize = 64;   // Length of signal
 
     // Define time vector
-    let mut time: Vec<f32> = vec![0.0; SIGNAL_LENGTH];
+    let mut time: Vec<f64> = vec![0.0; SIGNAL_LENGTH];
 
     // Various functions. Treat as complex signals, even if they're not. It's an interface requirement
-    let mut sin_fn: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); time.len()];  // sin(2*pi*t)
-    let mut cos_fn: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); time.len()];  // cos(2*pi*t)
-    let mut step_fn: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); time.len()];  // Step function
+    let mut sin_fn: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); time.len()];  // sin(2*pi*t)
+    let mut cos_fn: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); time.len()];  // cos(2*pi*t)
+    let mut step_fn: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); time.len()];  // Step function
 
     // Populate vectors
     for i in 0..SIGNAL_LENGTH {
-        let f = i as f32;
-        let sl = SIGNAL_LENGTH as f32;
+        let f = i as f64;
+        let sl = SIGNAL_LENGTH as f64;
         time[i] = f / sl;  // Normalization to make trigonometric calculations nicer
         let radians = time[i] * 2.0 * PI;
         sin_fn[i].re = radians.sin();
@@ -36,8 +36,17 @@ fn main() {
     }
 
     // Execute FFT
-    let mut _freq: Vec<Complex<f32>>;
-    _freq = fft::fft(sin_fn);
-    _freq = fft::fft(cos_fn);
-    _freq = fft::fft(step_fn);
+    let sampling_freq: f64 = time.len() as f64 / time[time.len() - 1];
+    let mut res = fft::fft(sin_fn, sampling_freq);
+    for i in 0..res.0.len() {
+        debug!("freq = {}, mag = {}", res.0[i], res.1[i]);
+    }
+    res = fft::fft(cos_fn, sampling_freq);
+    for i in 0..res.0.len() {
+        debug!("freq = {}, mag = {}", res.0[i], res.1[i]);
+    }
+    res = fft::fft(step_fn, sampling_freq);
+    for i in 0..res.0.len() {
+        debug!("freq = {}, mag = {}", res.0[i], res.1[i]);
+    }
 }
